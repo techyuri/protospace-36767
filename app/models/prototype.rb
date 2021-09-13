1c4fd7adb30a5944ba5d682e58d76c6ce56eb76f
techyuri <89583984+techyuri@users.noreply.github.com>
2021-09-13 21:07:27 +0900

    テーブルとカラムの作成

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,1 +1,2 @@
-
+class Prototype < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_105243) do
+ActiveRecord::Schema.define(version: 2021_09_13_115724) do
+
+  create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.text "concept", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_09_13_105243) do
     t.datetime "remember_created_at"
   end
 
+  add_foreign_key "prototypes", "users"
 end
