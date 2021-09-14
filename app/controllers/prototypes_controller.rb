9f13a16ab8b09b6030f630a05e7e60ec35eb93d5
techyuri <89583984+techyuri@users.noreply.github.com>
2021-09-14 11:56:14 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,8 +2,31 @@ class PrototypesController < ApplicationController
   def index
   end
   def new
+    @prototype = Prototype.new
   end
+
   def create
+    @prototype =Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+  
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
+
 
 end
